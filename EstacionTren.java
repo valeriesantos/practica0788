1ce1ebf584db0ff88a642efae43dc336a255054a
Valeriesantos <valnika96@gmail.com>
2016-10-21 14:05:31 +0200

    0178.b: insertar nuevo metodo ventaBilletes

--- EstacionTren.java
+++ EstacionTren.java
@@ -1,20 +1,33 @@
 public class EstacionTren
 {
-	private TicketMaquina maquina1;
-	private TicketMaquina maquina2;
-	private int totalDinero;
+    private TicketMaquina maquina1;
+    private TicketMaquina maquina2;
+    private int totalDinero;
 
-	public EstacionTren()
-	{
-		maquina1 = new TicketMaquina(300);
-		maquina2 = new TicketMaquina(200);
-		totalDinero= 0;
+    public EstacionTren()
+    {
+        maquina1 = new TicketMaquina(300);
+        maquina2 = new TicketMaquina(200);
+        totalDinero= 0;
   }
   
   public void imprimirDineroTotal()
-	{
-		totalDinero = maquina1.obtenerTotal();
-		totalDinero = totalDinero + maquina2.obtenerTotal();
+    {
+        totalDinero = maquina1.obtenerTotal();
+        totalDinero = totalDinero + maquina2.obtenerTotal();
     System.out.println(totalDinero);
-	}
-}	
+    }
+    
+    public void ventaBillete()
+     {
+        maquina1.insertarDinero(300);
+        maquina2.insertarDinero(200);
+        maquina1.imprimirTicket();
+        maquina2.imprimirTicket();
+        }
+    
+
+    
+
+}   
+
