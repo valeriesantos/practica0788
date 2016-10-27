cfd80f8ac0f9c024236e13c1182b3daa864a8817
Valeriesantos <valnika96@gmail.com>
2016-10-27 10:42:06 +0200

    0345.a: fijar precio

--- TicketMaquina.java
+++ TicketMaquina.java
@@ -23,9 +23,9 @@ public class TicketMaquina
      * Note that the price must be greater than zero, and there
      * are no checks to ensure this.
      */
-    public TicketMaquina(int coste)
+    public TicketMaquina()
     {
-        precio = coste;
+        precio = 800;
         balance = 0;
         total = 0;
     }
